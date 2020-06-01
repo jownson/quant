ffe6762fbe5b5c00d0a0ebd6586670ca9ceeec12
zqs <jownson@hotmail.com>
2020-06-01 17:56:31 +0800

    add hellword

---
+++ helloword.py
@@ -0,0 +1,44 @@
+'''
+Created on 2019年9月25日
+
+@author: Administrator
+'''
+
+if True:
+    print("Answer")
+    print("True")
+else:
+    print("Answer")
+    print("False")  # 缩进不一致，会导致运行错误
+
+# !/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+'''
+import tkinter
+top = tkinter.Tk()
+# 进入消息循环
+top.mainloop()
+
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
